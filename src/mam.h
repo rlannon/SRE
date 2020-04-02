1d1020ec3af5ea47a8eac3dd986f17dc3e4fe07b
Riley <lannon.riley@gmail.com>
2020-04-01 21:53:51 -0500

    Continue header work

--- src/mam.h
+++ src/mam.h
@@ -5,7 +5,9 @@ Copyright 2020 Riley Lannon
 
 Memory Allocation Manager (MAM)
 
-The MAM plays a crucial role in the SRE
+The MAM plays a crucial role in the SRE, particularly in memory safety. Specifically, it implements the allocation and release of dynamic memory from the operating system.
+The MAM contains a set with the addresses of all dynamic memory that has been allocated (all addresses returned by the C function malloc(), which is used to implement SIN's 'alloc dynamic'). This allows it to ensure the program never attempts to call free() on an address which was not returned by malloc().
+Further, it handles all the interaction between the program and the OS, reducing the opportunities for unsafe memory management practices.
 
 */
 
@@ -13,10 +15,12 @@ The MAM plays a crucial role in the SRE
 #define MAM_H
 
 #include <stdbool.h>
+#include "set.h"
 
 // Define the MAM struct
 typedef struct mam {
     // contains a hash table (more like a _set_) to store the addresses of memory obtained with malloc
+    set *s;
 };
 
 // Initialization and clean-up routines
--- src/sre.h
+++ src/sre.h
@@ -6,7 +6,7 @@ Copyright 2020 Riley Lannon
 Contains function definitions for the SIN runtime environment.
 
 Some of the SRE is implemented in C because it would be a waste of time, and more error-prone, to do it in ASM.
-Much of the assembly present in the runtime environment and standard library is simply to serve as a wrapper to C functions
+Much of the assembly present in the runtime environment and standard library is simply to serve as a wrapper to C functions.
 
 */
 
@@ -18,7 +18,9 @@ Much of the assembly present in the runtime environment and standard library is 
 #include "runtime_error_codes.h"
 #include "mam.h"
 
-// Initialization functions; called on program startup
+// Initialization and clean-up functions; called on program startup and exit
+void sre_init();
+void sre_clean();
 
 // SIN runtime errors
 void sinl_rte_index_out_of_bounds();
