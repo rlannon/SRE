6bb95adae3ef985e147de391c2861dde49508257
rlannon <lannon.riley@gmail.com>
2020-05-28 18:38:46 -0500

    Update str_copy return value This prevents reference invalidation

--- src/asm/string.s
+++ src/asm/string.s
@@ -17,9 +17,12 @@ sinl_str_copy:
     ;   ptr<string> src     -   the source string
     ;   ptr<string> dest    -   the destination string
     ; returns:
-    ;   bool    -   0 if failure, 1 if success
+    ;   ptr<string>     -   The address of the destination string
     ;
     ; According to the SIN calling convention, 'src' will be in RSI and 'dest' will be in RDI
+    ; 
+    ; Note that the destination string *must* be returned by this function in case the string needs to be reallocated
+    ; This prevents the reference to the string (variable on the stack, for example) from being invalidated
 
     ; compare the lengths; see if we need to reallocate the destination string
     mov eax, [rsi]
@@ -27,8 +30,11 @@ sinl_str_copy:
     cmp eax, ebx
     jg .copy
 
+; if we need to reallocate the string, this will execute
+.reallocate:
     ; todo: check to see if we need to reallocate here? the MAM will do it upon request for reallocation...
-    ; if the MAM actually has enough memory, the request for reallocation will ultimately do nothing
+    ; note the *actual* size of the allocated block for the string is in the MAM
+    ; if the MAM actually has enough memory for the new string, the request for reallocation will do nothing
 
     ; get the new size -- should be size of the source string * 1.5 for safety
     mov eax, [rsi]
@@ -39,8 +45,8 @@ sinl_str_copy:
     ; reallocate the destination string -- use the SRE
     ;   pass old address in RDI
     ;   pass new size in RSI
-    mov r12, rsi    ; first, preserve pointer values in registers -- r12 - r15 are "owned" by the caller
-    mov r13, rdi
+    mov r12, rsi    ; first, preserve pointer values in registers
+    mov r13, rdi    ; r12-r15 are considered "owned" by the caller and must be restored if used by the callee
 
     ; pass addesses
     mov rdi, [rdi]
@@ -50,7 +56,13 @@ sinl_str_copy:
     ; restore pointer values
     mov rsi, r12
     mov rdi, r13
+
+    ; the new destination address is in RAX; copy it into RDI
+    mov rdi, rax
+    
+; the actual copy routine
 .copy:
+    mov rax, rdi    ; ensure the address of the destination string is preserved in RAX
     cld ; ensure the direction flag is clear (so 'rep' increments the pointers)
     mov ecx, [rsi]
     movsd   ; copy the length information
